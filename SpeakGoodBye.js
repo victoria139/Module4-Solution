c5eb9757ef27be3d7c305a1e675b19134280e58d
victoria139 <99456545+victoria139@users.noreply.github.com>
2022-02-18 11:21:29 -0500

    Create SpeakGoodBye.js

---
+++ SpeakGoodBye.js
@@ -0,0 +1,11 @@
+/***Immediately Invoked Function Exression (IIFE)***/
+(function (window){
+    var byeSpeaker = {};
+    var speakWord = "Good Bye";
+    
+    byeSpeaker.speak = function (names) {
+      console.log(speakWord + " " + names);
+    }
+     
+     window.byeSpeaker = byeSpeaker;
+    })(window);
